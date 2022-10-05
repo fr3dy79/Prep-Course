3ba0cabc3e31ff8ca327448081951c2499b2987e
fredy poveda <fr3dypov3da@gmail.com>
2022-10-05 12:12:51 -0500

    02-JS-I/homework/homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,9 +4,9 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "79" ;
+const nuevoNum = "8" ;
 // Crea una variable booleana:
-const nuevoBool = "false";
+const nuevoBool = "water";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - "5" === 5;
